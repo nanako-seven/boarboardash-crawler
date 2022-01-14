226c91517208ae8160b48503114cbf68231fd4c4
nanako4 <anguuan@outlook.com>
2022-01-14 19:08:48 +0800

    添加注释

--- models.py
+++ models.py
@@ -51,11 +51,7 @@ class Image(ormar.Model):
 
 
 async def init_database():
-    # create the database
-    # note that in production you should use migrations
-    # note that this is not required if you connect to existing database
     engine = sqlalchemy.create_engine(config.database_url)
-    # just to be sure we clear the db before
     metadata.drop_all(engine)
     metadata.create_all(engine)
     time = datetime.datetime.now() - datetime.timedelta(minutes=60)
